12a3abe8080f3bafdef2370b3b776d3330a22f39
tomoki1207 <tomoki1207@gmail.com>
2016-05-16 08:27:40 +0900

    date translate

--- data/japanizer-for-slack.js
+++ data/japanizer-for-slack.js
@@ -10,56 +10,72 @@
 
   var texts = {
 
-    // ログインページ
-    'Product' : 'プロダクト',
-    'Pricing' : '価格',
-    'Support' : 'サポート',
-    'Blog' : 'ブログ',
-    'Sign in' : 'サインイン',
-    'Create a new team' : '新たなチームを作成',
-    'Enter your email address and password' : 'e-mailアドレスとパスワードを入力',
-    'Keep me signed in' : 'サインインしたままにする',
-    'I fogot my password' : 'パスワードを忘れた場合',
+    // Sign in page
+    'Product': 'プロダクト',
+    'Pricing': '価格',
+    'Support': 'サポート',
+    'Blog': 'ブログ',
+    'Sign in': 'サインイン',
+    'Create a new team': '新たなチームを作成',
+    'Enter your email address and password': 'e-mailアドレスとパスワードを入力',
+    'Keep me signed in': 'サインインしたままにする',
+    'I fogot my password': 'パスワードを忘れた場合',
 
-    // メインページ - menu
-    'Notifications' : '通知',
-    'Profile & account' : 'プロフィール設定',
-    'Preferences' : '表示設定',
-    "\n						Set yourself to " : 'モード変更 ',
-    "\n						[Away] Set yourself to " : '[不在] モード変更 ',
-    'away' : '不在',
-    'active' : 'アクティブ',
-    'Help & feedback' : 'ヘルプ & フィードバック',
-    'Invite people' : 'メンバーの招待',
-    'Manage team members' : 'メンバーの管理',
-    'Team settings' : 'チーム設定',
-    "\n					Apps & integrations\n				" : 'アプリ & 外部サービス連携',
-    'Customize Slack' : 'Slackのカスタマイズ',
-    'Statistics' : '統計情報',
-    'Billing' : 'プラン',
-    ' Sign in to another team …' : '別チームへサインイン',
-    "\n											CHANNELS\n											" : 'チャネル',
-    'Browse all channels' : 'すべてのチャネルを表示',
-    'DIRECT MESSAGES ' : 'ダイレクトメッセージ',
-    'Direct Messages' : 'ダイレクトメッセージ',
-    'Open a Direct Message' : 'ダイレクトメッセージを開く',
-    ' Invite People' : '招待',
+    // Main page - menu
+    'Notifications': '通知',
+    'Profile & account': 'プロフィール設定',
+    'Preferences': '表示設定',
+    "\n						Set yourself to ": 'モード変更 ',
+    "\n						[Away] Set yourself to ": '[不在] モード変更 ',
+    'away': '不在',
+    'active': 'アクティブ',
+    'Help & feedback': 'ヘルプ & フィードバック',
+    'Invite people': 'メンバーの招待',
+    'Manage team members': 'メンバーの管理',
+    'Team settings': 'チーム設定',
+    "\n					Apps & integrations\n				": 'アプリ & 外部サービス連携',
+    'Customize Slack': 'Slackのカスタマイズ',
+    'Statistics': '統計情報',
+    'Billing': 'プラン',
+    ' Sign in to another team …': '別チームへサインイン …',
+    "\n											CHANNELS\n											": 'チャネル',
+    'Browse all channels': 'すべてのチャネルを表示',
+    'DIRECT MESSAGES ': 'ダイレクトメッセージ',
+    'Direct Messages': 'ダイレクトメッセージ',
+    'Open a Direct Message': 'ダイレクトメッセージを開く',
+    ' Invite People': '招待',
 
-    'Today' : '今日',
-    'Yesterday' : '昨日',
-    '(edited)' : '(編集済)',
+    // Main page - messages
+    ' (edited)': '(編集済)',
 
 
-    'Narrow your search' : '絞込検索',
+    'Narrow your search': '絞込検索',
   };
 
   var placeholders = {
     'Search': '検索',
-    'Search channels' : 'チャネル検索',
-    'Find or start a conversation' : '検索 & チャット開始'
+    'Search channels': 'チャネル検索',
+    'Find or start a conversation': '検索 & チャット開始'
   };
 
   var titles = {};
+
+  var dates = {
+    'January': '1',
+    'February': '2',
+    'March': '3',
+    'April': '4',
+    'May': '5',
+    'June': '6',
+    'July': '7',
+    'August': '8',
+    'September': '9',
+    'October': '10',
+    'November': '11',
+    'December': '12',
+    'Today': '今日',
+    'Yesterday': '昨日'
+  };
 
   // ------------------ Translate functions --------------------------
 
@@ -102,6 +118,17 @@
     }
   };
 
+  var translateDayDivider = function(node) {
+    var monthandday = node.data.split(' ');
+    console.log(node.data);
+    if (monthandday.length !== 2){
+      node.data = dates[node.data];
+    } else {
+      // parseInt で序数を削除
+      node.data = dates[monthandday[0]] + '/' + parseInt(monthandday[1]);
+    }
+  };
+
   var translateNode = function(node) {
 
     // 単純置換可能なテキスト
@@ -126,6 +153,12 @@
     result = document.evaluate('.//*[@title]', node, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, result);
     for (var l = 0, lLength = result.snapshotLength; l < lLength; ++l) {
       translateAttribute(result.snapshotItem(l), 'title', titles);
+    }
+
+    // 日付
+    result = document.evaluate('.//*[@class="day_divider_label"]/text()', node, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, result);
+    for (var m = 0, mLength = result.snapshotLength; m < mLength; ++m) {
+      translateDayDivider(result.snapshotItem(m));
     }
   };
 
